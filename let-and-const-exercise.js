6c1d8db7c878a388bdd6ce90a78ba7cb0e525fe8
mistemberg <101603348+mistemberg@users.noreply.github.com>
2023-05-16 21:45:21 -0400

    Add files via upload

---
+++ let-and-const-exercise.js
@@ -0,0 +1,17 @@
+//ES5 Global Constants, using const where reassignment is not permited. 
+//ES2015 let and const were introduced 
+
+const PI = 3.14;
+let x = 10;
+
+// - What is the difference between ***var*** and ***let***?
+// A: Both are used to declare variables, the main key difference is in terms of scoping and hoisting. 'var' is function-scoped and subject to hoisting, while 'let' is block-scoped and not hoisted 
+// - What is the difference between **var** and ***const***?
+// A: 'var' can declare and reassigned variables, while 'const' declare unchangeable value that cannot be reassigned.
+// - What is the difference between **let** and **const**?
+// A: 'let' allows for variable reassignment, while 'const' creates variables with a constant value that cannot be reassigned 
+// - What is hoisting?
+// A: hoisting is a behavior where variable and function declarations are moved to the top of their containing scope during the compilation phase. You can use variables and functions before they are declared.
+
+
+
